d8008ad41c14637c9860c4e4a045b11c42c53a21
Parth <65162182+pbisen@users.noreply.github.com>
2022-04-06 16:01:39 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Blockchain@BITS'
+copyright = '2022, BITS Pilani'
+author = 'Blockchain Society'
 
 release = '0.1'
 version = '0.1.0'
